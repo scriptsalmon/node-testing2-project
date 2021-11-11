5a811eb5b11035ea82a5cd055359b00bb22d79cf
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2021-11-11 13:56:33 -0800

    get by id and add endpoints fleshed out. now, the gf wants attention.

--- api/archetypes/archetypes-model.js
+++ api/archetypes/archetypes-model.js
@@ -3,6 +3,7 @@ const db = require("../../data/dbConfig.js");
 module.exports = {
   get,
   getById,
+  add
 };
 
 function get() {
@@ -10,5 +11,10 @@ function get() {
 }
 
 function getById(id) {
-  return db("archetypes").where("archetypes_id", id);
+  return db("archetypes").where("archetype_id", id);
+}
+
+async function add(type) {
+    const [id] = await db('archetypes').insert(type)
+    return getById(id);
 }
--- api/archetypes/archetypes-router.js
+++ api/archetypes/archetypes-router.js
@@ -10,4 +10,25 @@ router.get('/', (req, res, next) => {
       .catch(next)
 })
 
+router.get('/:id', (req, res, next) => {
+    Archetypes.getById(req.params.id)
+      .then(data => {
+          res.status(200).json(data)
+      })
+      .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+    const { archetype_name, archetype_description } = req.body;
+    if(!archetype_name || !archetype_description){
+        next({ status: 400, message: "name and description required" })
+    } else {
+        Archetypes.add(req.body)
+          .then(data => {
+              res.status(201).json(data)
+          })
+          .catch(next);
+    }
+})
+
 module.exports = router;
